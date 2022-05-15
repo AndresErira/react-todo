0e0ea569e6d52156a99ea0ebc82a28b4144e86d1
Andres Erira Lenovi <haeh2007@gmail.com>
2022-05-15 14:24:37 -0500

    Se agrega manejo de estado a todoSearch con el hook useState

--- src/TodoSearh.js
+++ src/TodoSearh.js
@@ -2,16 +2,40 @@ import React from "react";
 import './TodoSearch.css';
 
 function TodoSearch(){
+/**
+ * Para manejar el estado en react se llama a
+ * React.useState();----->agrega el estado a los
+ * componentes cuando se crean como funciones
+ * el useState es un hook de react
+ * los hooks se reconocen porque anteceden la palabra use
+ * ejemplos:    useEfect, useState
+ 
+    const [state, setState] = React.useState('Andres');
+    el primer valor es el estado y el segundo es la funcion
+    que lo modifica
+    */
+   const [searchValue, setSearchValue]=React.useState('');
+   /**
+    * por convencion el segundo parametro se le antepone set pero
+    * podria llevar cualquier nombre
+    * se puede convinar con un evento para que este desde una 
+    * funcion modifique el estado
+    */
     const onSearchValueChange = (e)=>{
-        console.log(e.target.value);
+        
+        setSearchValue(e.target.value);
     }
-    return(
+    return[
         <input
+            key={1}
             className="input-search"
             placeholder="Busca un Todo"
+            value={searchValue}
             onChange={onSearchValueChange}
-        />
-    )
+        />,
+        <p
+        key={2}>{searchValue}</p>
+    ]
 }
 export {TodoSearch};
 /**
